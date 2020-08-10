9e05bf615fbaad525a41d0442b3404fc62e91f8d
Brandan Sayarath <brandan@brandan.tech>
2020-08-10 13:45:41 -0700

    add ApprovalCard component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React from "react";
 
-
-const CommentDetail = props => {
+const CommentDetail = (props) => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
@@ -10,14 +9,14 @@ const CommentDetail = props => {
       <div className="content">
         <a href="/" className="author">
           {props.author}
-         </a>
+        </a>
         <div className="metadata">
           <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">{props.commentText}</div>
+        <div className="text">{props.content}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
- return (
-   <div className="ui container comments">
-     <CommentDetail author="Sam" timeAgo="Today at 4:45 PM" commentText="Cool bro" avatar={faker.image.avatar()} />
-     <CommentDetail author="Jane" timeAgo="Today at 2:45 PM" commentText="Amazing" avatar={faker.image.avatar()} />
-     <CommentDetail author="Alex" timeAgo="Yesterday at 2:15 PM" commentText="Nice" avatar={faker.image.avatar()} />
-   </div>
+  return (
+    <div className="ui container comments">
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 4:45 PM"
+        content="Cool bro"
+        avatar={faker.image.avatar()}
+      />
 
- )
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 2:45 PM"
+        content="Amazing"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Yesterday at 2:15 PM"
+        content="Nice"
+        avatar={faker.image.avatar()}
+      />
+    </div>
+  );
 };
 
-
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
